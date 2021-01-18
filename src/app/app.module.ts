14a6a74e41f67b223e5986917845c9ae4705ae9d
Karen <bbsuen@hotmail.co.uk>
2021-01-18 21:11:32 +0000

    Generated 5 components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ArrayFormComponent } from './array-form/array-form/array-form.component';
+import { BasicFormComponent } from './basic-form/basic-form/basic-form.component';
+import { NestedFormComponent } from './nested-form/nested-form/nested-form.component';
+import { SubmitFormComponent } from './submit-form/submit-form/submit-form.component';
+import { ValidFormComponent } from './valid-form/valid-form/valid-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArrayFormComponent,
+    BasicFormComponent,
+    NestedFormComponent,
+    SubmitFormComponent,
+    ValidFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
