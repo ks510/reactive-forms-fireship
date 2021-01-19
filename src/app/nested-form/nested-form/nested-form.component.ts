4ecc7feeb59f3459d1254ce191307ce73a438741
Karen <bbsuen@hotmail.co.uk>
2021-01-19 16:47:43 +0000

    Finished nested form example

--- src/app/nested-form/nested-form/nested-form.component.ts
+++ src/app/nested-form/nested-form/nested-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-nested-form',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NestedFormComponent implements OnInit {
 
-  constructor() { }
+  myForm: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    // reusable form group
+    const phone = this.fb.group({
+      area: [],
+      prefix: [],
+      line: [],
+    })
+
+    // parent form
+    this.myForm = this.fb.group({
+      email: '',
+      homePhone: phone,
+      cellPhone: phone,
+    })
   }
 
 }
